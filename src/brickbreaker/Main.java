ce5ad5a0bf469e51d1f3ca2bee5e3b9271945cc7
NaveenJoyGit <naveenjoy.mec@gmail.com>
2024-03-15 14:44:41 +0530

    My First Project

---
+++ src/brickbreaker/Main.java
@@ -0,0 +1,16 @@
+package brickbreaker;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
+        obj.setBounds(10, 10, 600, 700);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+    }
+}
